dba51dae1eb41619d0271a2f5cd975ef8eca4db2
david durost <david.durost@gmail.com>
2014-09-21 12:54:41 -0400

    update to work with SyliusResourceBundle

--- VolleyballOrganizationBundle.php
+++ VolleyballOrganizationBundle.php
@@ -1,6 +1,35 @@
 <?php
 namespace Volleyball\Bundle\OrganizationBundle;
 
-class VolleyballOrganizationBundle extends \Symfony\Component\HttpKernel\Bundle\Bundle
+use \Symfony\Component\DependencyInjection\ContainerBuilder;
+use \Sylius\Bundle\ResourceBundle\AbstractResourceBundle;
+use \Sylius\Bundle\ResourceBundle\SyliusResourceBundle;
+
+class VolleyballOrganizationBundle extends AbstractResourceBundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public static function getSupportedDrivers()
+    {
+        return array(
+            SyliusResourceBundle::DRIVER_DOCTRINE_ORM
+        );
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    protected function getBundlePrefix()
+    {
+        return 'volleyball_organization';
+    }
 }
